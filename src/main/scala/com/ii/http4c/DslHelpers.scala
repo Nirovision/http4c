181d1ea12e0ccac3ddd2da307234a605249222f6
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2016-11-07 09:15:28 +1100

    feat(matchpathvar) - Added a matcher for performing validation on paths. It allows you to handle the failure case explicitly

--- src/main/scala/com/ii/http4c/DslHelpers.scala
+++ src/main/scala/com/ii/http4c/DslHelpers.scala
@@ -12,8 +12,16 @@ object DslHelpers {
     def unapply(str: String): Option[A] = f(str)
   }
 
-  def pathMatcher[A](f: String => Option[A]) = {
+  def pathMatcher[A](f: String => Option[A]): MatchPathVar[A] = {
     MatchPathVar(f)
+  }
+
+  def validatingPathMatcher[A](f: String => ParseFailure \/ A): MatchPathVar[ParseFailure \/ A] = {
+    val v: String => Option[ParseFailure \/ A] = f(_) match {
+      case \/-(a)   => Some(a.right)
+      case -\/(e) => Some(e.left)
+    }
+    MatchPathVar(v)
   }
 
   def queryParamDecoderFromEither[A](f: String => ParseFailure \/ A): QueryParamDecoder[A] = {
