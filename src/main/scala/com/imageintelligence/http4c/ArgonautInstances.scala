a91e64c33806396609a93c9dfb1a6ac4fff2ce30
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2016-11-23 12:38:33 +1100

    feat(decoder): Added an implicit helper to automatically deserialize any A with a DecodeJson[A]

--- src/main/scala/com/imageintelligence/http4c/ArgonautInstances.scala
+++ src/main/scala/com/imageintelligence/http4c/ArgonautInstances.scala
@@ -1,5 +1,11 @@
 package com.imageintelligence.http4c
 
+import argonaut.DecodeJson
+import org.http4s.EntityDecoder
 import org.http4s.argonaut.{ArgonautInstances => Ai}
 
-object ArgonautInstances extends Ai
+object ArgonautInstances extends Ai {
+  implicit def decoderFromDecodeJson[A](implicit codec: DecodeJson[A]): EntityDecoder[A] = {
+    jsonOf(codec)
+  }
+}
