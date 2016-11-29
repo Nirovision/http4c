b19720fc714cdc85a604e13adff3bb31febbf0e6
Dominic Bou-Samra <dominic.bou.samra@gmail.com>
2016-11-29 14:38:54 +1100

    feat(preserveorder): Encoder for Json preserves order now

--- src/main/scala/com/imageintelligence/http4c/ArgonautInstances.scala
+++ src/main/scala/com/imageintelligence/http4c/ArgonautInstances.scala
@@ -1,11 +1,26 @@
 package com.imageintelligence.http4c
 
+import argonaut.Argonaut
 import argonaut.DecodeJson
+import argonaut.Json
+import org.http4s.Charset
 import org.http4s.EntityDecoder
+import org.http4s.EntityEncoder
+import org.http4s.MediaType
 import org.http4s.argonaut.{ArgonautInstances => Ai}
+import org.http4s.headers.`Content-Type`
 
 object ArgonautInstances extends Ai {
   implicit def decoderFromDecodeJson[A](implicit codec: DecodeJson[A]): EntityDecoder[A] = {
     jsonOf(codec)
   }
+
+  override implicit val jsonEncoder: EntityEncoder[Json] =
+    EntityEncoder.stringEncoder(Charset.`UTF-8`).contramap[Json] { json =>
+      // TODO naive implementation materializes to a String.
+      // Look into replacing after https://github.com/non/jawn/issues/6#issuecomment-65018736
+      Argonaut.nospace.copy(preserveOrder = true).pretty(json)
+
+
+    }.withContentType(`Content-Type`(MediaType.`application/json`, Charset.`UTF-8`))
 }
